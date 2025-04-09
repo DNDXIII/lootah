6e70066e954a00a86bc988ce5b37a947a41070d4
didier <didier.n.dias@gmail.com>
2025-04-09 17:34:54 +0200

    enemy health bar

---
+++ Assets/Scripts/UI/WorldHealthDisplay.cs
@@ -0,0 +1,47 @@
+﻿using Gameplay.Shared;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class WorldHealthDisplay : MonoBehaviour
+    {
+        [Header("Settings")] [SerializeField] private bool hideWhenFull = true;
+
+        [Header("References")] [SerializeField] [Tooltip("The health bar GameObject that will be shown/hidden.")]
+        private GameObject healthBarGameObject;
+
+        [SerializeField] [Tooltip("The health bar image that will be filled based on the health ratio.")]
+        private Image healthBarImage;
+
+        [SerializeField] private BaseHealth health;
+
+        private void Start()
+        {
+            health.OnHealed += _ => UpdateHealthBar();
+            health.OnDamaged += (_, _) => UpdateHealthBar();
+            health.OnDie += () => ToggleHealthBar(false);
+
+
+            UpdateHealthBar();
+            // Set the health bar to be active or inactive based on the current health ratio
+            ToggleHealthBar(!hideWhenFull);
+        }
+
+        private void ToggleHealthBar(bool active)
+        {
+            healthBarGameObject.SetActive(active);
+        }
+
+        private void UpdateHealthBar()
+        {
+            healthBarImage.fillAmount = health.GetHealthRatio();
+            Debug.Log($"fillAmount: {healthBarImage.fillAmount}");
+
+            if (hideWhenFull)
+            {
+                ToggleHealthBar(health.GetHealthRatio() < 1f);
+            }
+        }
+    }
+}
